ac45211a26b535a0bf3b5f7218ed4961bedcc4ea
giualz <giulia.alzuguir@gmail.com>
2021-08-11 12:12:24 -0300

    validation

--- app/routes/routeForm.js
+++ app/routes/routeForm.js
@@ -1,6 +1,8 @@
 const {controllerGetForm} = require("../controllers/controllerForm");
+const middlewarDtoValidate = require("../utils/dtoValidade");
 
 module.exports = (routerForm) => {
     routerForm.route("/form")
-    .get(controllerGetForm);
+    .get(controllerGetForm)
+    .post(middlewarDtoValidate("body", controllerGetForm.postFormSchema), controllerGetForm.postFormSchema)
 }
